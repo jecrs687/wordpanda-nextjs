a9b5d80ae0904ce58e3a1c838036bc386d9dfedb
Emanuel Cascone <45917859+jecrs687@users.noreply.github.com>
2024-01-24 10:54:18 -0300

    Delete src/Containers directory

--- src/Containers/hooks/useCookie.tsx
+++
@@ -1,9 +0,0 @@
-import { getCookie } from "@utils/cookie";
-
-export const useCookie = (name: string | string[]) => {
-    if (typeof window === 'undefined') return;
-    if (Array.isArray(name)) return name.map((name) => getCookie(name));
-
-    return getCookie(name);
-
-}
--- src/Containers/hooks/useModal.tsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useModal = create((set) => ({
-    modal: false,
-    toggle: (modal) => set((state) => ({ modal: modal })),
-}));
-
-export default useModal;
