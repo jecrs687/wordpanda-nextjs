2cb5a6424fb71e6bbaf3829814269cc6cbe726a8
Emanuel Cascone <emanuelcascone@gmail.com>
2024-01-25 10:57:13 -0300

    fix: envs

---
+++ prisma/migrations/20240124152657_/migration.sql
@@ -0,0 +1,252 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'BKO', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "MediaType" AS ENUM ('MOVIE', 'VIDEO', 'AUDIO');
+
+-- CreateTable
+CREATE TABLE "Translation" (
+    "id" SERIAL NOT NULL,
+    "word_id" INTEGER NOT NULL,
+    "language_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Translation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "first_name" VARCHAR(90),
+    "last_name" VARCHAR(90),
+    "username" TEXT,
+    "phone" TEXT,
+    "email" VARCHAR(90),
+    "password" VARCHAR(120),
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "last_login_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_language" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "language_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "user_language_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_word" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+    "wordId" INTEGER NOT NULL,
+    "user_language_id" INTEGER NOT NULL,
+    "attemps" INTEGER NOT NULL DEFAULT 0,
+    "errors" INTEGER NOT NULL DEFAULT 0,
+    "streak" INTEGER NOT NULL DEFAULT 0,
+    "last_attemp" TIMESTAMP(3),
+    "last_error" TIMESTAMP(3),
+    "last_success" TIMESTAMP(3),
+    "not_learned" BOOLEAN DEFAULT true,
+    "progress" INTEGER DEFAULT 0,
+    "quality" INTEGER DEFAULT 0,
+    "interval" INTEGER DEFAULT 0,
+    "next_attemp" TIMESTAMP(3),
+
+    CONSTRAINT "user_word_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Word" (
+    "id" SERIAL NOT NULL,
+    "word" TEXT NOT NULL,
+    "languageId" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+    "difficulty" INTEGER DEFAULT 0,
+    "attemps" INTEGER DEFAULT 0,
+    "errors" INTEGER DEFAULT 0,
+
+    CONSTRAINT "Word_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Platform" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "logo_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Platform_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Media" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "type" "MediaType" NOT NULL DEFAULT 'VIDEO',
+    "platform_id" INTEGER NOT NULL,
+    "logo_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MediaLanguages" (
+    "id" SERIAL NOT NULL,
+    "media_id" INTEGER NOT NULL,
+    "language_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "MediaLanguages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MediaWords" (
+    "id" SERIAL NOT NULL,
+    "word_id" INTEGER NOT NULL,
+    "media_language_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "MediaWords_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MediaUser" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "progress" INTEGER DEFAULT 0,
+    "wordsLearned" INTEGER DEFAULT 0,
+    "media_language_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "MediaUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Language" (
+    "id" SERIAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Language_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_translationsTo" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_language_user_id_language_id_key" ON "user_language"("user_id", "language_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_word_user_id_user_language_id_wordId_key" ON "user_word"("user_id", "user_language_id", "wordId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Word_word_languageId_key" ON "Word"("word", "languageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Word_id_languageId_key" ON "Word"("id", "languageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MediaLanguages_media_id_language_id_key" ON "MediaLanguages"("media_id", "language_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MediaWords_word_id_media_language_id_key" ON "MediaWords"("word_id", "media_language_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MediaUser_user_id_media_language_id_key" ON "MediaUser"("user_id", "media_language_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Language_code_key" ON "Language"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_translationsTo_AB_unique" ON "_translationsTo"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_translationsTo_B_index" ON "_translationsTo"("B");
+
+-- AddForeignKey
+ALTER TABLE "Translation" ADD CONSTRAINT "Translation_word_id_fkey" FOREIGN KEY ("word_id") REFERENCES "Word"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Translation" ADD CONSTRAINT "Translation_language_id_fkey" FOREIGN KEY ("language_id") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_language" ADD CONSTRAINT "user_language_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_language" ADD CONSTRAINT "user_language_language_id_fkey" FOREIGN KEY ("language_id") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_word" ADD CONSTRAINT "user_word_wordId_fkey" FOREIGN KEY ("wordId") REFERENCES "Word"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_word" ADD CONSTRAINT "user_word_user_language_id_fkey" FOREIGN KEY ("user_language_id") REFERENCES "user_language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Word" ADD CONSTRAINT "Word_languageId_fkey" FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_platform_id_fkey" FOREIGN KEY ("platform_id") REFERENCES "Platform"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaLanguages" ADD CONSTRAINT "MediaLanguages_media_id_fkey" FOREIGN KEY ("media_id") REFERENCES "Media"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaLanguages" ADD CONSTRAINT "MediaLanguages_language_id_fkey" FOREIGN KEY ("language_id") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaWords" ADD CONSTRAINT "MediaWords_word_id_fkey" FOREIGN KEY ("word_id") REFERENCES "Word"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaWords" ADD CONSTRAINT "MediaWords_media_language_id_fkey" FOREIGN KEY ("media_language_id") REFERENCES "MediaLanguages"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaUser" ADD CONSTRAINT "MediaUser_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MediaUser" ADD CONSTRAINT "MediaUser_media_language_id_fkey" FOREIGN KEY ("media_language_id") REFERENCES "MediaLanguages"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_translationsTo" ADD CONSTRAINT "_translationsTo_A_fkey" FOREIGN KEY ("A") REFERENCES "Translation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_translationsTo" ADD CONSTRAINT "_translationsTo_B_fkey" FOREIGN KEY ("B") REFERENCES "Word"("id") ON DELETE CASCADE ON UPDATE CASCADE;
