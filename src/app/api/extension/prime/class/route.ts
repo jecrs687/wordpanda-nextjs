55c28e588e8612c55197ad3a0c027a08b43d043b
Emanuel Cascone <emanuelcascone@gmail.com>
2024-02-03 10:48:33 -0300

    feat: prime class

---
+++ src/app/api/extension/prime/class/route.ts
@@ -0,0 +1,10 @@
+export async function GET(request: Request) {
+    return Response.json({
+        data: {
+            className: 'ffdnc7h f1m37uyw f1hxvvt7 atvwebplayersdk-subtitleoption-text'
+        },
+        err: null,
+        msg: 'Languages fetched'
+    })
+}
+
