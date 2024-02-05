fb965c35416bd6790b81115af96f2766a321ca6f
Emanuel Cascone <emanuelcascone@gmail.com>
2024-02-05 04:50:35 -0300

    fix: migrate

--- src/app/(common)/migrate/page.tsx
+++ src/app/(common)/migrate/page.tsx
@@ -1,21 +1,18 @@
-import { createUser } from "@backend/domain/actions/User/createUser.action"
-import { migrateLanguages } from "@infra/database/migration/languages"
-import { subtitleMock } from "@infra/database/migration/subtitles"
 
 export default async function Page() {
     //await migrateDatabase()
-    await migrateLanguages()
+    // await migrateLanguages()
 
-    await createUser({
-        email: "demo@demo.it",
-        firstName: "demo",
-        lastName: "demo",
-        password: "Demo123!",
-        phone: "1234567890",
-        username: "demo",
-        languageId: 606
-    })
-    await subtitleMock()
+    // await createUser({
+    //     email: "demo@demo.it",
+    //     firstName: "demo",
+    //     lastName: "demo",
+    //     password: "Demo123!",
+    //     phone: "1234567890",
+    //     username: "demo",
+    //     languageId: 606
+    // })
+    // await subtitleMock()
 
     // await createUser({
     //     email: "admin@admin.com",
