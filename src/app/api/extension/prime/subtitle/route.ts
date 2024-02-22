70566249b97a48e4f092172b02f8b908cc8e393b
Emanuel Cascone <emanuelcascone@gmail.com>
2024-02-22 08:22:46 -0300

    fix: image link

--- src/app/api/extension/prime/subtitle/route.ts
+++ src/app/api/extension/prime/subtitle/route.ts
@@ -12,12 +12,12 @@ export async function POST(request: Request) {
 
     const body: ExtensionPrimeSubtitlePostRequest = await request.json();
     const { platform, links, name, image } = body;
-    const validateImage = "https://images-na.ssl-images-amazon.com"
+    const validateImage = ["https://images-na.ssl-images-amazon.com", "https://m.media-amazon.com"]
     const validateLinks = "https://cf-timedtext.aux.pv-cdn.net"
     if (!platform || !links || !name || !image) {
         return Response.json({ err: 'Invalid data' });
     }
-    if (!image.includes(validateImage)) {
+    if (!validateImage.some(image.includes)) {
         return Response.json({ err: 'Invalid image' });
     }
     if (!links.every(link => link.includes(validateLinks))) {
