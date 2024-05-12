91b5603d3d51447cd402fce5ebaf6ec90bea0bbe
Emanuel Cascone <emanuelcascone@gmail.com>
2024-05-12 18:00:48 +0200

    feat: order of cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,10 +41,10 @@ export async function middleware(request: NextRequest) {
     })
     response.headers.append('Access-Control-Allow-Credentials', "true")
     response.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    response.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT,OPTIONS')
+    response.headers.append('Access-Control-Allow-Methods', '*')
     response.headers.append(
         'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        '*'
     )
     return response
 }
