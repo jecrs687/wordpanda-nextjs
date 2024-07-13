81d1266c7b1ea40010aad0c3f32ced1791268877
Emanuel Cascone <emanuelcascone@gmail.com>
2024-07-13 17:37:19 +0200

    fix: inject endpoint and favicon

--- src/app/api/extension/prime/subtitle/route.ts
+++ src/app/api/extension/prime/subtitle/route.ts
@@ -1,6 +1,5 @@
 import { IInsertSubtitles, insertSubtitles } from "@backend/domain/actions/Subtitles/insertSubtitles";
 import { deepcopy } from "@utils/deepcopy";
-import fs from 'fs';
 export type ExtensionPrimeSubtitlePostRequest = IInsertSubtitles
 export type ExtensionPrimeSubtitlePostResponse = {
     err?: string | null,
@@ -23,10 +22,10 @@ export async function POST(request: Request) {
     if (!links.every(link => link.url.includes(validateLinks))) {
         return Response.json({ err: 'Invalid links' });
     }
-    const mocks = fs.readFileSync('./src/app/api/extension/prime/subtitle/mocks.json', 'utf-8',);
-    const mock = JSON.parse(mocks);
-    if (!mock.some(x => x.mediaId === body.mediaId))
-        fs.writeFileSync('./src/app/api/extension/prime/subtitle/mocks.json', JSON.stringify([...mock, body], null, 2), 'utf-8');
+    // const mocks = fs.readFileSync('./src/app/api/extension/prime/subtitle/mocks.json', 'utf-8',);
+    // const mock = JSON.parse(mocks);
+    // if (!mock.some(x => x.mediaId === body.mediaId))
+    //     fs.writeFileSync('./src/app/api/extension/prime/subtitle/mocks.json', JSON.stringify([...mock, body], null, 2), 'utf-8');
     requests.push(body);
     if (requests.length < 20) {
         clearTimeout(timeout);
