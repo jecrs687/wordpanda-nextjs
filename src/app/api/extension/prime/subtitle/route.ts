ffb1da16b8aeb70b6e74d9d195aa3aa19f727b24
Emanuel Cascone <emanuelcascone@gmail.com>
2024-07-18 00:09:05 +0200

    fix: promise pool import

--- src/app/api/extension/prime/subtitle/route.ts
+++ src/app/api/extension/prime/subtitle/route.ts
@@ -1,6 +1,7 @@
 import { IInsertSubtitles, insertSubtitles } from "@backend/domain/actions/Subtitles/insertSubtitles";
 import { deepcopy } from "@utils/deepcopy";
-import { envs } from "@utils/envs";
+import { promisePoll } from "@utils/promisePoll";
+import { readFileSync, writeFileSync } from "fs";
 export type ExtensionPrimeSubtitlePostRequest = IInsertSubtitles
 export type ExtensionPrimeSubtitlePostResponse = {
     err?: string | null,
@@ -8,6 +9,47 @@ export type ExtensionPrimeSubtitlePostResponse = {
 }
 const requests = [];
 let timeout;
+let flag = false;
+async function processRequests() {
+    if (flag) return;
+    flag = true;
+    let inc = 0;
+    let err = 0;
+    console.log(`Processing ${requests.length} requests`);
+    await promisePoll<IInsertSubtitles>(async (x) => {
+        requests.shift();
+        const results = readFileSync('./src/app/api/extension/prime/subtitle/mocks1.json', 'utf-8',);
+        const data = JSON.parse(results);
+        if (data.some((y) => y.mediaId === x.mediaId)) {
+            requests.shift();
+            return;
+        }
+        const result = await insertSubtitles([deepcopy(x)])
+        if (result.success) {
+            inc++
+            const results = readFileSync('./src/app/api/extension/prime/subtitle/mocks1.json', 'utf-8',);
+            const data = JSON.parse(results);
+            data.push(x);
+            writeFileSync('./src/app/api/extension/prime/subtitle/mocks1.json', JSON.stringify(data, null, 2));
+
+        } else {
+            requests.push(x);
+            err++
+        };
+        console.log({
+            success: result.success,
+            totSuccess: inc,
+            totError: err
+        })
+        return;
+    }, requests, 12);
+    console.log('Finished');
+    flag = false;
+    if (requests.length > 0) processRequests();
+}
+
+
+
 export async function POST(request: Request) {
 
     const body: ExtensionPrimeSubtitlePostRequest = await request.json();
@@ -23,18 +65,12 @@ export async function POST(request: Request) {
     if (!links.every(link => link.url.includes(validateLinks))) {
         return Response.json({ err: 'Invalid links' });
     }
-    // const mocks = fs.readFileSync('./src/app/api/extension/prime/subtitle/mocks.json', 'utf-8',);
-    // const mock = JSON.parse(mocks);
-    // if (!mock.some(x => x.mediaId === body.mediaId))
-    //     fs.writeFileSync('./src/app/api/extension/prime/subtitle/mocks.json', JSON.stringify([...mock, body], null, 2), 'utf-8');
-    requests.push(body);
-    if (requests.length < 20) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            insertSubtitles(deepcopy(requests));
-            requests.length = 0;
-        }, +envs.POOL_INSERT_SUBTITLE_TIMEOUT || 60000)
-    }
+    const mocks = readFileSync('./src/app/api/extension/prime/subtitle/mocks.json', 'utf-8',);
+    const data = JSON.parse(mocks);
+    if (requests.length === 0)
+        data.forEach((x) => requests.push(x))
+    processRequests();
+
     return Response.json({ msg: 'OK' });
 }
 
