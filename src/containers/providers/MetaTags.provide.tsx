af4a12d26263253f0ba917ea5a8f4b7101ac3a44
Emanuel Cascone <emanuelcascone@gmail.com>
2024-07-21 00:32:41 +0200

    feat: ezoic metatag

--- src/containers/providers/MetaTags.provide.tsx
+++ src/containers/providers/MetaTags.provide.tsx
@@ -1,6 +1,7 @@
 
 export function MetaTagsProvider() {
     return (<>
+        <meta name="ezoic-site-verification" content="HyY2CK8LAALeDeFYiwVlbTStYov30I" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
